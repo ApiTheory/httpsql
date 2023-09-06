da3aa6e3820eb545ce718c281a58d61e324a10ab
Alex Gadea <154999+agad@users.noreply.github.com>
2023-09-05 22:25:57 -0400

    added tests for SqlCommand.preTransactionVariableSubstitution()

--- test/sql-command.test.js
+++ test/sql-command.test.js
@@ -58,4 +58,65 @@ describe('SqlCommand', () => {
     }).to.throw('the sql command object can not be validated')
   })
 
+  it('executable command array should match params array if no vars submitted', () => {
+    const c = new SqlCommand( { ...basicSqlCommand, ...{ params: [ 1, '{lastop.rows.1.name}'] } } )
+    //c.preTransactionVariableSubstitution( { id: 5, name: 'testname' })
+    c.preTransactionVariableSubstitution()
+    expect(c._executableParams).deep.equal([ 1, '{lastop.rows.1.name}'])
+    expect(c._params).deep.equal([ 1, '{lastop.rows.1.name}'])
+    expect(c._finalizedParams).deep.equal([])
+  })
+
+  it('executable command array should properly represent substituted variables', () => {
+    const c = new SqlCommand( { ...basicSqlCommand, ...{ params: [ 1, 'testval', '{variable.status}', '{name}', '{lastop.rows.1.name}'] } } )
+    c.preTransactionVariableSubstitution( { id: 5, name: 'testname', status: 'active' })
+    expect(c._executableParams).deep.equal([ 1, 'testval', 'active', 'testname', '{lastop.rows.1.name}'])
+    expect(c._params).deep.equal([ 1, 'testval', '{variable.status}', '{name}', '{lastop.rows.1.name}'])
+    expect(c._finalizedParams).deep.equal([])
+  })
+
+  it('executable command array should substitute null if not strict and variable not found', () => {
+    const c = new SqlCommand( { ...basicSqlCommand, ...{ params: [ 1, 'testval', '{variable.status}', '{name}', '{lastop.rows.1.name}'] } } )
+    c.preTransactionVariableSubstitution( { id: 5, name: 'testname' })
+    expect(c._executableParams).deep.equal([ 1, 'testval', null, 'testname', '{lastop.rows.1.name}'])
+    expect(c._params).deep.equal([ 1, 'testval', '{variable.status}', '{name}', '{lastop.rows.1.name}'])
+    expect(c._finalizedParams).deep.equal([])
+  })
+
+  it('should throw if a dynamic parameter variable is a number', () => {
+    const c = new SqlCommand( { ...basicSqlCommand, ...{ params: [ 1, '{variable.status}', '{7}', '{lastop.rows.1.name}'] } } )
+    expect(() => {
+      c.preTransactionVariableSubstitution( { id: 5, name: 'testname', status: 'active' })
+    }).to.throw(`the dynamic parameter '{7}' is a number`)
+  })
+
+  it('should throw if a dynamic parameter lastop is a number', () => {
+    const c = new SqlCommand( { ...basicSqlCommand, ...{ params: [ 1, '{variable.status}', '{lastop.3}'] } } )
+    expect(() => {
+      c.preTransactionVariableSubstitution( { id: 5, name: 'testname', status: 'active' })
+    }).to.throw(`the dynamic parameter '{lastop.3}' is a number`)
+  })
+
+  it('should throw if a dynamic parameter results is a number', () => {
+    const c = new SqlCommand( { ...basicSqlCommand, ...{ params: [ 1, '{variable.status}', '{results.99}'] } } )
+    expect(() => {
+      c.preTransactionVariableSubstitution( { id: 5, name: 'testname', status: 'active' })
+    }).to.throw(`the dynamic parameter '{results.99}' is a number`)
+  })
+
+  
+  it('should throw if a dynamic parameter result is not properly named', () => {
+    const c = new SqlCommand( { ...basicSqlCommand, ...{ params: [ 1, '{variable.status}', '{badvar.test}'] } } )
+    expect(() => {
+      c.preTransactionVariableSubstitution( { id: 5, name: 'testname', status: 'active' })
+    }).to.throw(`dynamic parameter '{badvar.test}' can not be properly parsed`)
+  })
+
+  it('should throw if strict and a dynamic parameter variable can not be aligned with something passed', () => {
+    const c = new SqlCommand( { ...basicSqlCommand, ...{ strict: true, params: [ 1, '{variable.status}' ] } } )
+    expect(() => {
+      c.preTransactionVariableSubstitution( { id: 5, name: 'testname' })
+    }).to.throw(`parameter '{variable.status}' not found`)
+  })
+
 })
