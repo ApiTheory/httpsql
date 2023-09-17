0eb957c6c4e4340c7c724ff560a637becad58be0
Alex Gadea <154999+agad@users.noreply.github.com>
2023-09-16 22:32:41 -0400

    arrayToObject function

--- src/util.js
+++ src/util.js
@@ -23,3 +23,19 @@ export const isPlainObject =  val  => {
 	const prototype = Object.getPrototypeOf( val );
 	return ( prototype === null || prototype === Object.prototype || Object.getPrototypeOf(prototype) === null) && !(Symbol.toStringTag in val ) && !(Symbol.iterator in val );
 }
+
+
+/**
+ * Converts an array of objects into an object, using a specified key field as the object keys.
+ *
+ * @param {Array} array - The array of objects to convert.
+ * @param {string} keyField - The name of the key field in each object.
+ * @return {Object} - The resulting object with the specified key field as the key for the extracted object.
+ */
+export const arrayToObject = (array, keyField) =>
+   array.reduce((obj, item) => {
+     obj[item[keyField]] = item
+     return obj
+   }, {})
+
+  
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { isString, isNumeric, getDirName, isPlainObject } from '../src/util.js'
+import { isString, isNumeric, getDirName, isPlainObject, arrayToObject } from '../src/util.js'
 import { Root } from '../src/root.js'
 
 describe('utils', () => {
@@ -45,6 +45,22 @@ describe('utils', () => {
       expect(isString([])).false
     })
 
+  })
+
+  describe('arrayToObject()', () => { 
+   
+    it('should convert an array of objects into an object, using a specified key field as the object keys.', () => {
+      expect(arrayToObject([ 
+        { id: 1, name: 'test1', status: 'active' }, 
+        { id: 2, name: 'test2', status: 'complete' },
+        { id: 3, name: 'test3', status: 'complete' } 
+      ], 'name')).deep.equals({ 
+        test1: { id: 1, name: 'test1', status: 'active' }, 
+        test2: { id: 2, name: 'test2', status: 'complete' }, 
+        test3: { id: 3, name: 'test3', status: 'complete' } 
+      })
+    })
+    
   })
 
   describe('isPlainObject()', () => {
