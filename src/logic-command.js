d82c0d8598cf033d2bea0b866f731169c0a4948f
Alex Gadea <154999+agad@users.noreply.github.com>
2023-09-16 22:06:20 -0400

    adjustments to classes to change over to jsonata

--- src/logic-command.js
+++ src/logic-command.js
@@ -1,17 +1,20 @@
 
 import Ajv from 'ajv'
-import * as fs from 'fs'
-import path from 'path'
-import { getDirName } from './util.js'
-import { Command } from './command.js'
-import { CommandValidationError, LogicOpFailureError } from './errors.js'
-import { LogicEngine } from 'json-logic-engine'
-import { logicOpSchema } from './json-schemas/logicop-command-schema.js'
+import jsonata from 'jsonata'
 
-const logicEngine = new LogicEngine()
+import { Command } from './command.js'
+import { CommandValidationError, LogicOpFailureError, ExpectationFailureError } from './errors.js'
+import { logicOpSchema } from './json-schemas/logicop-command-schema.js'
+import { isPlainObject  } from './util.js'
+
 const ajv = new Ajv({ allErrors: true, strict: false })
 const logicOpCommandValidator = ajv.compile( logicOpSchema )
 
+/**
+ * Executes a logic operation and returns true and false as the 'value'.  There is nothing else to evaluate.  If the value=true,
+ * the context can move on to the next command.  If the value=false, its onExpectationFailure time.  The expectation will always
+ * be that the value is true.
+ */
 export class LogicCommand extends Command {
 
   constructor( command ) {
@@ -23,42 +26,78 @@ export class LogicCommand extends Command {
 
     const { logicOp, ...opts } = command
 
+    // this is what the expectation is ALWAYS for logic ops, basically, whatever command goes through MUST be true
+    opts.expect = 'value = true'
+    opts.onExpectationFailure = opts.onExpectationFailure || 'throw'
     super( logicOp, opts )
 
-    this._onFailure = opts.onFailure || 'throw'
+    // test the operation to make sure it's well formed.  This will NOT check the validity of parameters, just that
+    // the operation itself is well formed jsonata
+    try {
+      jsonata( logicOp )
+    } catch ( err ) {
+      throw new CommandValidationError( `the logic operation is not well formed: ${err.message}`)
+    }
 
   }
 
-  async execute( results ) {
+  async execute( contextSnapshot ) {
 
-    if( logicEngine.run( this._command , { 
-      lastop: structuredClone(results[ results.length -1 ]), 
-      results : structuredClone( results ) 
-    } ) ) {
+    const evalResult = {}
+
+    try {
+
+      const expression = jsonata( this._command )
+      evalResult.value = await expression.evaluate( contextSnapshot )
       
+    } catch ( err ) {
+
+      throw new LogicOpFailureError( err.message )
+      
+    } 
+
+    if ( evalResult.value ) {
       return { status : 'success' }
+    }
+
+    if ( isPlainObject(this.onExpectationFailure)) {
+
+      const { message:customMessage, code, ...additionalData } = this.onExpectationFailure
+
+      return { 
+        error: new ExpectationFailureError( customMessage || `the logic operation: '${this._command}' failed`, this._expectationDescription , code, additionalData ),
+        status: 'expectation-failure', 
+        failureAction: 'throw'
+      }
+
+    } else if ( this.onExpectationFailure === 'stop') {
+      
+      return { 
+        error: new ExpectationFailureError( `the logic operation: '${this._command}' failed`, this._expectationDescription  ),
+        status: 'expectation-failure', 
+        failureAction: this.onExpectationFailure
+      
+      }
 
     } else {
 
-      if ( this._onFailure === 'stop' ) {
-
-        return { status : 'stop' }
-
-      } else if ( this._onFailure.message ) {
-        const { message, code, ...additionalData } = this._onFailure
-        throw new LogicOpFailureError( message, code, additionalData )
-      } else {
-
-        throw new LogicOpFailureError( )
-
+      return { 
+        error: new ExpectationFailureError( `the logic operation: '${this._command}' failed`, this._expectationDescription  ),
+        status: 'expectation-failure', 
+        failureAction: this.onExpectationFailure 
       }
-
     }
+
+
+   
+
+  } 
+
+
+
   
-  }
-  
-  get type () {
-    return 'logic' 
+  get type() {
+    return 'logic'
   }
 
 }
