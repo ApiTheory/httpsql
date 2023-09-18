12a0ca9d68cc9ffa089b23eba7d3e4418d625415
Alex Gadea <154999+agad@users.noreply.github.com>
2023-09-18 10:16:40 -0400

    export sqliteDataDriver and pgDataDriver

--- index.js
+++ index.js
@@ -3,4 +3,7 @@
 
 import { TransactionManager } from './src/transaction-manager.js'
 import { Root as HttpSqlRoot } from './src/root.js'
-export { TransactionManager, HttpSqlRoot  }
+import { SqliteDataDriver } from './src/data-drivers/sqlite-driver.js'
+import { PgDataDriver } from './src/data-drivers/pg-driver.js'
+export default TransactionManager
+export { TransactionManager, HttpSqlRoot, SqliteDataDriver, PgDataDriver }
