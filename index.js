302c22b0213be78caf6644751edfb4729aa49ba4
Alex Gadea <154999+agad@users.noreply.github.com>
2023-09-22 19:00:58 -0400

    export DatabaseError

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ import { TransactionManager } from './src/transaction-manager.js'
 import { Root as HttpSqlRoot } from './src/root.js'
 import { SqliteDataDriver } from './src/data-drivers/sqlite-driver.js'
 import { PgDataDriver } from './src/data-drivers/pg-driver.js'
+import { DatabaseError } from './src/errors.js'
 export default TransactionManager
-export { TransactionManager, HttpSqlRoot, SqliteDataDriver, PgDataDriver }
+export { TransactionManager, HttpSqlRoot, SqliteDataDriver, PgDataDriver, DatabaseError }
